58235ffc58fc5558a391fe2400c04640339d5b68
bbcsc <bbcsc@LAPTOP-QVJH7I2R>
2018-06-03 14:26:35 +0800

    d

--- src/test/java/com/bbc/MavenTestngDemo/AppTest.java
+++ src/test/java/com/bbc/MavenTestngDemo/AppTest.java
@@ -1,11 +1,28 @@
 package com.bbc.MavenTestngDemo;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AppTest { 
+	boolean tip = true;
 	@Test
-	public void test() {
+	public void test2() {
 		System.out.println("first test");
+		
+		Assert.assertEquals(tip, false);
+		
+	}
+	@Test
+	public void test3() {
+		Assert.assertEquals(tip, true);
+	}
+	@Test
+	public void test4() {
+		Assert.assertEquals(tip, false);
+	}
+	@Test
+	public void test5() {
+		Assert.assertEquals(tip, true);
 	}
 	
 }
---
+++ src/test/java/com/bbc/MavenTestngDemo/mvnTestNg.java
@@ -0,0 +1,10 @@
+package com.bbc.MavenTestngDemo;
+
+import org.testng.annotations.Test;
+
+public class mvnTestNg {
+	@Test
+	public void testHelloworld() {
+		System.out.println("helloworld2");
+	}
+}
